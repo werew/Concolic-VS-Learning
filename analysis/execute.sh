5cf1018110e70f05abf8fb17eaffe26010f8a08c
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2018-03-23 19:48:54 +0100

    Log into stats

--- analysis/execute.sh
+++ analysis/execute.sh
@@ -4,4 +4,6 @@ CLASSPATH=../Learning/build:
 CLASSPATH+=../Learning/lib/learnlib.jar:
 CLASSPATH+=.
 
-java -cp "$CLASSPATH" $1 $2 $3 $4 $5
+#cat Test.java | grep runControlledExperiment >> stats.txt
+date >> stats.txt
+java -cp "$CLASSPATH" $1 $2 $3 $4 $5 | tee -a stats.txt
